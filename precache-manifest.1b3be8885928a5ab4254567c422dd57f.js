c5004bba71875de18d39bb43a16256a31aaf6224
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-06-21 16:01:18 +0200

    Updates

---
+++ precache-manifest.1b3be8885928a5ab4254567c422dd57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a71adf9cba64ef205f1572cd7012e4",
+    "url": "/ListApp/index.html"
+  },
+  {
+    "revision": "00e94f0e1ce71fca744e",
+    "url": "/ListApp/static/js/2.59136ad9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ListApp/static/js/2.59136ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38535dc7d06001b39fcf",
+    "url": "/ListApp/static/js/main.68214376.chunk.js"
+  },
+  {
+    "revision": "f4c1f8663afe47eaaf7d",
+    "url": "/ListApp/static/js/runtime-main.7670a290.js"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/ListApp/static/media/plus.e43ce6c8.svg"
+  }
+]);
